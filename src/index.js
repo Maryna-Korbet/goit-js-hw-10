7760ee8a255958b57a328b87355d2c6403cc0401
Maryna-Korbet <m22v86k@gmail.com>
2022-08-15 03:46:28 +0300

    decoration to the interface elements

--- src/index.js
+++ src/index.js
@@ -11,14 +11,23 @@ const refs = {
     input: document.querySelector('#search-box'),
     list: document.querySelector('.country-list'),
     info: document.querySelector('.country-info'),
+    body: document.querySelector('body'),
 }
 
 refs.input.addEventListener('input', debounce(searchCountries, DEBOUNCE_DELAY));
 
+for (let e of document.getElementsByTagName("body")) {
+    e.style.color = "#ffffff";
+    e.style.background = "#7a6450";
+    e.style.backgroundImage = "url('https://cdn.pixabay.com/photo/2017/07/22/11/46/adventure-2528477_960_720.jpg')";
+    e.style.backgroundRepeat = "no-repeat";
+    e.style.backgroundSize = "cover";
+    }
+
 function searchCountries(e) {
     const nameCountry = e.target.value.trim();
     if (nameCountry === '') {
-        return;
+        return (refs.list.innerHTML = ''), (refs.info.innerHTML = '');
     }
     fetchCountries(nameCountry)
         .then(data => {
